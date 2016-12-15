0cd0a4be6479bbbd6bba99e3ee787e8bf93d7156
vgupta0786 <hbticob@gmail.com>
2016-12-15 17:18:45 +0530

    4th commit for rock paper scissor exercise.
    4th commit for rock paper scissor exercise.

--- RockPaperScissorProblem.java
+++ RockPaperScissorProblem.java
@@ -3,11 +3,18 @@ package exercise1;
 public class RockPaperScissorProblem {
 
     public int compareChoices(String firstSelection, String secondSelection) {
+        SelectionType firstChoice,secondChoice;
+        int result=1;
         if(null==SelectionType.getSelectedObjectByName(firstSelection)||
-        null==SelectionType.getSelectedObjectByName(secondSelection))
-            throw new RuntimeException();
+        null==SelectionType.getSelectedObjectByName(secondSelection)){
+            throw new RuntimeException("Please input correct parameters");
+        }else{
+             firstChoice=SelectionType.getSelectedObjectByName(firstSelection);
+             secondChoice=SelectionType.getSelectedObjectByName(secondSelection);
+        }
+        if(firstChoice.equals(secondChoice))
+            result=0;
         
-        
-        return 1;
+        return result;
     }
 }
