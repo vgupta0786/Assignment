150a55ef18f3198802fc9bf7c63ef6119c52a473
vgupta0786 <hbticob@gmail.com>
2016-12-15 17:27:28 +0530

    6th commit for rock paper scissor exercise.
    6th commit for rock paper scissor exercise.

--- RockPaperScissorProblem.java
+++ RockPaperScissorProblem.java
@@ -1,5 +1,8 @@
 package exercise1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RockPaperScissorProblem {
 
     public int compareChoices(String firstSelection, String secondSelection) {
@@ -14,6 +17,11 @@ public class RockPaperScissorProblem {
         }
         if(firstChoice.equals(secondChoice))
             result=0;
+        
+        Map<SelectionType,SelectionType> hm=new HashMap<SelectionType,SelectionType>();
+        hm.put(SelectionType.ROCK, SelectionType.SCISSORS);
+        if(firstChoice != secondChoice)
+            return hm.get(firstChoice) == secondChoice ? 1 : -1;
         
         return result;
     }
