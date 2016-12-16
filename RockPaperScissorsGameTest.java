80ebb99f986087a772aa7889311ef1718335b2dd
vgupta0786 <hbticob@gmail.com>
2016-12-16 18:02:56 +0530

    review comments incorporated version5
    review comments incorporated version5

--- RockPaperScissorsGameTest.java
+++ RockPaperScissorsGameTest.java
@@ -2,6 +2,7 @@ package exercise1;
 
 import static exercise1.Options.ROCK;
 import static exercise1.Options.SCISSORS;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -18,6 +19,8 @@ public class RockPaperScissorsGameTest {
      */
     private static final RockPaperScissorsGame game = new RockPaperScissorsGame();
 
+    private static final String TIE="tie";
+
     /**
      * test playGame method returns not null object
      */
@@ -25,17 +28,22 @@ public class RockPaperScissorsGameTest {
     public final void whenInputsArePassedThanResultShouldNotBeNull() {
         assertNotNull(game.playGame(ROCK, SCISSORS));
     }
-    
+
     /**
-     * test playGame method returns not null object
+     * test playGame method returns GameResult object
      */
     @Test
     public final void whenInputsArePassedThanResultShouldBeGameResult() {
         assertTrue(game.playGame(ROCK, SCISSORS) instanceof GameResult);
-     }
-    
-   
-    
-   
+    }
+
+    /**
+     * test same inputs are given than result should be tie
+     */
+    @Test
+    public final void whenSameInputsAreSelectedThanResultShouldBeTie() {
+        assertEquals(game.playGame(ROCK, ROCK).getResult(),
+                RockPaperScissorsGameTest.TIE);
+    }
 
 }
