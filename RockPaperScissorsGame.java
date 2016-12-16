6ea213fb3a1fb3789e7d48bcb75801b294946a77
vgupta0786 <hbticob@gmail.com>
2016-12-16 18:05:16 +0530

    review comments incorporated version8
    review comments incorporated version8

--- RockPaperScissorsGame.java
+++ RockPaperScissorsGame.java
@@ -1,5 +1,9 @@
 package exercise1;
 
+import java.util.HashMap;
+import java.util.Map;
+import static exercise1.Options.*;
+
 /**
  * RockPaperScissorsGame to decide winner based on players selection
  * 
@@ -17,8 +21,18 @@ public class RockPaperScissorsGame {
      */
     public GameResult playGame(Options frstSelection, Options scndSelection) {
         GameResult result = new GameResult();
-        if (frstSelection==scndSelection)
+        if (frstSelection == scndSelection) {
             result.setResult(GameConstants.TIE);
+            return result;
+        }
+        Map<Options, Options> container = new HashMap<Options, Options>();
+        container.put(ROCK, SCISSORS);
+        container.put(SCISSORS, PAPER);
+        container.put(PAPER, ROCK);
+        if (container.get(frstSelection) == scndSelection)
+            result.setResult(GameConstants.FIRST_PLAYER_WIN);
+        else
+            result.setResult(GameConstants.SECOND_PLAYER_WIN);
         return result;
     }
 
