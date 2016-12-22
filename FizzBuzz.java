a3e2a774fc281f521cc9441918e4d0e778b2130a
vgupta0786 <hbticob@gmail.com>
2016-12-22 12:19:10 +0530

    FizzBuzz Problem first commit
    setting up test for testing print stream results and test for least possible input parameter

---
+++ FizzBuzz.java
@@ -0,0 +1,17 @@
+package exercise3;
+
+/**
+ * @author vgup77
+ *
+ */
+public class FizzBuzz {
+
+    /**
+     * accepts integer and prints appropriate result
+     * @param i
+     */
+    public void printResult(int i) {
+        System.out.print(i);
+    }
+
+}
---
+++ FizzBuzzTest.java
@@ -0,0 +1,55 @@
+package exercise3;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ * @author vgup77
+ *
+ */
+public class FizzBuzzTest {
+    
+    private ByteArrayOutputStream outContent;
+    private ByteArrayOutputStream errContent;
+    private FizzBuzz fizzBuzz;
+
+    /**
+     * instantiations
+     */
+    @Before
+    public void setUpStreams() {
+        outContent = new ByteArrayOutputStream();
+        errContent = new ByteArrayOutputStream();
+        fizzBuzz = new FizzBuzz();
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    /**
+     * for cleanup
+     */
+    @After
+    public void cleanUpStreams() {
+        outContent = null;
+        errContent = null;
+        fizzBuzz = null;
+        System.setOut(null);
+        System.setErr(null);
+    }
+    
+    /**
+     * test for least possible value
+     */
+    @Test
+    public void When1IsPassedItShouldPrint1(){
+        fizzBuzz.printResult(1);
+        assertEquals("1",outContent.toString());
+    }
+}
