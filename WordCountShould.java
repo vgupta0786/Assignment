086036beb7c24e63fc97ef0aaa3383fbf3af4926
vgupta0786 <hbticob@gmail.com>
2016-12-26 10:11:59 +0530

    Red- count 2 When 2 Same words Are Passed

--- WordCountShould.java
+++ WordCountShould.java
@@ -9,13 +9,19 @@ import org.junit.Test;
 
 public class WordCountShould {
 
-    
     @Test
     public void print_single_word_and_its_count_in_a_collection() {
         WordCount wordCount = new WordCount();
         Map<String, Integer> wordCountMap = new HashMap<String, Integer>();
         wordCountMap.put("hello", new Integer(1));
         assertEquals(wordCountMap, wordCount.countUniqueWords("hello"));
+    }
+
+    @Test
+    public void print_two_when_same_words_delimited_with_space_are_passed() {
+        WordCount wordCount = new WordCount();
+        Map<String,Integer> wordCountMap=wordCount.countUniqueWords("hello hello");
+        assertEquals(new Integer(2), wordCountMap.get("hello"));
     }
 
 }
