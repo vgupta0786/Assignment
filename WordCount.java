3f36df6b9eae4f52de20cd921e67151019ef92dd
vgupta0786 <hbticob@gmail.com>
2016-12-26 10:14:47 +0530

    Green - Split with delimiter and collect in a map

--- WordCount.java
+++ WordCount.java
@@ -1,14 +1,17 @@
 package exercise4;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
+import java.util.stream.Collectors;
 
 public class WordCount {
 
-    public Map<String,Integer> countUniqueWords(String word) {
-        Map<String,Integer> wordCountMap = new HashMap<String, Integer>();
-        wordCountMap.put("hello", new Integer(1));
+    public Map<String, Integer> countUniqueWords(String sentence) {
+        Map<String, Integer> wordCountMap = new HashMap<String, Integer>();
+        List<String> wordList = Arrays.asList(sentence.split(" "));
+        wordCountMap = wordList.stream().collect(Collectors.toMap(s -> s, s -> 1, Integer::sum));
         return wordCountMap;
     }
 
