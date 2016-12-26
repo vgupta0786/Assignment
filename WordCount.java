f24f73dcc117fe8e86266ce16701ba2a28eb39d1
vgupta0786 <hbticob@gmail.com>
2016-12-26 10:08:30 +0530

    Green - Grouped word and hardcoded count in a map

--- WordCount.java
+++ WordCount.java
@@ -1,9 +1,15 @@
 package exercise4;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class WordCount {
 
-    public String countUniqueWords(String word) {
-        return "1";
+    public Map<String,Integer> countUniqueWords(String word) {
+        Map<String,Integer> wordCountMap = new HashMap<String, Integer>();
+        wordCountMap.put("hello", new Integer(1));
+        return wordCountMap;
     }
 
 }
