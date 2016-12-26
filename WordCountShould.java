40359c41f13a2923225ca1921ff2142d106d1856
vgupta0786 <hbticob@gmail.com>
2016-12-26 10:16:54 +0530

    Additional test case to be sure
    print 2 diff words and their count

--- WordCountShould.java
+++ WordCountShould.java
@@ -20,8 +20,14 @@ public class WordCountShould {
     @Test
     public void print_two_when_same_words_delimited_with_space_are_passed() {
         WordCount wordCount = new WordCount();
-        Map<String,Integer> wordCountMap=wordCount.countUniqueWords("hello hello");
+        Map<String, Integer> wordCountMap = wordCount.countUniqueWords("hello hello");
         assertEquals(new Integer(2), wordCountMap.get("hello"));
     }
 
+    @Test
+    public void print_two_diff_words_and_count() {
+        WordCount wordCount = new WordCount();
+        Map<String, Integer> wordCountMap = wordCount.countUniqueWords("hi hello");
+        assertEquals(new Integer(1), wordCountMap.get("hello"));
+    }
 }
