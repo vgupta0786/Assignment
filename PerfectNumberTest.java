1acf11da7fa995ac58ee5c5cde04bfcedddb741b
vgupta0786 <hbticob@gmail.com>
2016-12-27 21:13:48 +0530

    Red - should_return_false_As_4_is_not_factorOf_6

--- PerfectNumberTest.java
+++ PerfectNumberTest.java
@@ -1,5 +1,6 @@
 package exercise5;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -15,6 +16,12 @@ public class PerfectNumberTest {
     public void should_return_true_As_1_is_factorOf_6() {
         PerfectNumber perfectNumber = new PerfectNumber();
         assertTrue(perfectNumber.isFactor(1,6));
+    }
+    
+    @Test
+    public void should_return_false_As_4_is_not_factorOf_6() {
+        PerfectNumber perfectNumber = new PerfectNumber();
+        assertFalse(perfectNumber.isFactor(4,6));
     }
 
 }
