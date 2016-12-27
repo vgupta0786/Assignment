6d18e526132e276d06aff5517b97ff75206c9980
vgupta0786 <hbticob@gmail.com>
2016-12-27 21:20:01 +0530

    Green - find out perfect number

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 public class PerfectNumber {
 
-    public boolean isPerfectNumber(int i) {
-        return true;
+    public boolean isPerfectNumber(int number) {
+        int sum = 0;
+        for (Integer factors : factorFor(number)) {
+            sum += factors.intValue();
+        }
+        return sum - number == number;
     }
 
     public boolean isFactor(int factor, int number) {
@@ -17,9 +21,9 @@ public class PerfectNumber {
         int factor = 1;
         List<Integer> factors = new ArrayList<Integer>();
         factors.add(1);
-        while(factor <= number/2){
-            if(isFactor(factor,number)){
-                factors.add(number/factor);
+        while (factor <= number / 2) {
+            if (isFactor(factor, number)) {
+                factors.add(number / factor);
             }
             factor++;
         }
