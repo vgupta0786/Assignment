7feb6126fc11dba36f88910d1326b19acd13d7cc
vgupta0786 <hbticob@gmail.com>
2016-12-29 11:29:41 +0530

    Green - return one for 1 else zero

--- NumberToWord.java
+++ NumberToWord.java
@@ -3,7 +3,10 @@ package cleancode;
 public class NumberToWord {
 
     public String numberToWord(int number) {
-        return "zero";
+        if(number==1)
+            return "one";
+        else
+            return "zero";
     }
 
 }
