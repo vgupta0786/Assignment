a1cc5258fd1ddeaed054b30e58ecb76d337e6509
vgupta0786 <hbticob@gmail.com>
2016-12-29 11:38:53 +0530

    Green - used map to pair number and word

--- NumberToWord.java
+++ NumberToWord.java
@@ -1,9 +1,16 @@
 package cleancode;
 
+import java.util.HashMap;
+
 public class NumberToWord {
 
     public String numberToWord(int number) {
-        return number == 1 ? "one" : "zero";
+        HashMap<Integer, String> numToWordMap=new HashMap<Integer, String>();
+        numToWordMap.put(0, "zero");
+        numToWordMap.put(1, "one");
+        numToWordMap.put(7, "seven");
+        
+        return numToWordMap.get(number);
     }
 
 }
