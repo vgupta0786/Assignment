bc03624a86862f062bed7f3cb312cb5bbc2a9cd9
vgupta0786 <hbticob@gmail.com>
2016-12-29 18:51:54 +0530

    Red -  return ninetynine for 99

--- NumberToWordShould.java
+++ NumberToWordShould.java
@@ -28,25 +28,30 @@ public class NumberToWordShould {
     public void print_seven_when_7_is_passed() {
         assertEquals("seven", convert.numberToWord(7));
     }
-    
+
     @Test
     public void print_nineteen_when_19_is_passed() {
         assertEquals("nineteen", convert.numberToWord(19));
     }
-    
+
     @Test
     public void print_twentyone_when_21_is_passed() {
         assertEquals("twentyone", convert.numberToWord(21));
     }
-    
+
     @Test
     public void print_twentynine_when_29_is_passed() {
         assertEquals("twentynine", convert.numberToWord(29));
     }
-    
+
     @Test
     public void print_thirty_when_30_is_passed() {
         assertEquals("thirty", convert.numberToWord(30));
+    }
+    
+    @Test
+    public void print_ninetynine_when_99_is_passed() {
+        assertEquals("ninetynine", convert.numberToWord(99));
     }
 
 }
