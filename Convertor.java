7d8eb94e39668569c36a7eda6a7957ad4a5b7f70
vgupta0786 <hbticob@gmail.com>
2016-12-29 11:48:46 +0530

    Green - handled in same if

--- Convertor.java
+++ Convertor.java
@@ -6,21 +6,23 @@ import java.util.List;
 
 public class Convertor {
 
-    private static final String TWENTY = "twenty";
-
-    private List<String> numToWordList = asList("zero", "one", "two", "three", "four", "five",
+    private final List<String> tensList = asList("", "", "twenty", "thirty"); 
+    
+    private List<String> onesList = asList("", "one", "two", "three", "four", "five",
             "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen",
             "fifteen", "sixteen", "seventeen", "eighteen", "nineteen");
 
     public String numberToWord(int number) {
-
-        if (number>=21 && number<=29) {
+        if(number==0)
+            return "zero";
+        
+        if (number>=21 && number<=30) {
             StringBuilder builder = new StringBuilder(50);
-            builder.append(Convertor.TWENTY);
-            builder.append(numToWordList.get(number % 10));
+            builder.append(tensList.get(number / 10));
+            builder.append(onesList.get(number % 10));
             return builder.toString();
         }
-        return numToWordList.get(number);
+        return onesList.get(number);
     }
 
 }
