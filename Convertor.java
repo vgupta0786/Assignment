3d8230fa7904301dedd278d7a98bec97ee421d9c
vgupta0786 <hbticob@gmail.com>
2016-12-29 11:43:59 +0530

    Green - passed using existing list

--- Convertor.java
+++ Convertor.java
@@ -1,15 +1,25 @@
 package cleancode;
 
 import static java.util.Arrays.asList;
+
 import java.util.List;
 
 public class Convertor {
+
+    private static final String TWENTY = "twenty";
 
     private List<String> numToWordList = asList("zero", "one", "two", "three", "four", "five",
             "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen",
             "fifteen", "sixteen", "seventeen", "eighteen", "nineteen");
 
     public String numberToWord(int number) {
+
+        if (number == 21) {
+            StringBuilder builder = new StringBuilder(50);
+            builder.append(Convertor.TWENTY);
+            builder.append(numToWordList.get(number % 10));
+            return builder.toString();
+        }
         return numToWordList.get(number);
     }
 
