5ad9255b6ef71bf87a2cd68f56b670061ba1f214
vgupta0786 <hbticob@gmail.com>
2016-12-29 11:30:44 +0530

    test Refactor - setup method to instantiate obj

--- NumberToWordShould.java
+++ NumberToWordShould.java
@@ -2,19 +2,25 @@ package cleancode;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class NumberToWordShould {
-
+    
+    private NumberToWord convert;
+    
+    @Before
+    public void setUp() {
+        convert = new NumberToWord();
+    }
+    
     @Test
     public void print_zero_when_0_is_passed() {
-        NumberToWord convert = new NumberToWord();
         assertEquals("zero", convert.numberToWord(0));
     }
 
     @Test
     public void print_one_when_1_is_passed() {
-        NumberToWord convert = new NumberToWord();
         assertEquals("one", convert.numberToWord(1));
     }
 
