bdb2ce79647c0e817a9609bf309caa4823e3db09
vgupta0786 <hbticob@gmail.com>
2016-12-29 11:32:41 +0530

    Green - used ternary inplace of ifelseif

--- NumberToWord.java
+++ NumberToWord.java
@@ -3,10 +3,7 @@ package cleancode;
 public class NumberToWord {
 
     public String numberToWord(int number) {
-        if(number==1)
-            return "one";
-        else
-            return "zero";
+        return number == 1 ? "one" : "zero";
     }
 
 }
