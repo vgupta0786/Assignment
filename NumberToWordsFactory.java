a1bef7a12c868da46d2aa75c4182a653dda5e0bb
vgupta0786 <hbticob@gmail.com>
2016-12-29 18:52:57 +0530

    Green - passed by changing limit

--- NumberToWordsFactory.java
+++ NumberToWordsFactory.java
@@ -2,11 +2,15 @@ package cleancode;
 
 public class NumberToWordsFactory {
 
-    public Convertor createinstance(int number) {
+    public Convertor createInstance(int number) {
 
         OnesConvertor onesConvertor = new OnesConvertor();
         TensConvertor tensConvertor = new TensConvertor();
 
-        return number >= 20 && number <= 30 ? tensConvertor : onesConvertor;
+        return isTwoDigit(number) ? tensConvertor : onesConvertor;
+    }
+
+    private boolean isTwoDigit(int number) {
+        return number >= 20 && number <= 99;
     }
 }
