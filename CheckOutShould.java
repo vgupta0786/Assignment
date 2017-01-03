5f0105a2d7c603ebb105dc04fa4ba30a15941f8a
vgupta0786 <hbticob@gmail.com>
2017-01-03 13:01:40 +0530

    Red - scan 2 items

--- CheckOutShould.java
+++ CheckOutShould.java
@@ -1,5 +1,6 @@
 package cleancode;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
@@ -10,5 +11,11 @@ public class CheckOutShould {
     public void scan_one_item() {
         CheckOut checkout = new CheckOut();
         assertNotNull(checkout.scanItem("A,50"));
+    }
+    
+    @Test
+    public void scan_2_items() {
+        CheckOut checkout = new CheckOut();
+        assertEquals(2,checkout.scanItem("A,50:B,30").size());
     }
 }
