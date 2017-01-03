dfc8891c2bf3fb3a722b67010dc1230cc66ce7f3
vgupta0786 <hbticob@gmail.com>
2017-01-03 13:11:02 +0530

    refactored previous failing test

--- CheckOutShould.java
+++ CheckOutShould.java
@@ -10,26 +10,26 @@ public class CheckOutShould {
     @Test
     public void scan_one_item() {
         CheckOut checkout = new CheckOut();
-        assertNotNull(checkout.scanItem("A,50"));
+        assertNotNull(checkout.scanItem("A,50,0,0"));
     }
 
     @Test
     public void scan_2_items() {
         CheckOut checkout = new CheckOut();
-        assertEquals(2, checkout.scanItem("A,50:B,30").size());
+        assertEquals(2, checkout.scanItem("A,50,0,0:B,30,0,0").size());
     }
 
     @Test
     public void scan_one_item_and_print_its_price() {
         CheckOut checkout = new CheckOut();
-        checkout.scanItem("A,50");
+        checkout.scanItem("A,50,0,0");
         assertEquals(new Integer(50), checkout.getTotalPrice());
     }
 
     @Test
     public void scan_2_items_and_print_total_price() {
         CheckOut checkout = new CheckOut();
-        checkout.scanItem("A,50:B,30");
+        checkout.scanItem("A,50,0,0:B,30,0,0");
         assertEquals(new Integer(80), checkout.getTotalPrice());
     }
 
