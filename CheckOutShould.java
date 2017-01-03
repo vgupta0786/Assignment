93a3e5cbe973638273f3c6f989cefa06b5f30a99
vgupta0786 <hbticob@gmail.com>
2017-01-03 13:09:13 +0530

    Red- scan 3 same items and prints offer price

--- CheckOutShould.java
+++ CheckOutShould.java
@@ -18,18 +18,25 @@ public class CheckOutShould {
         CheckOut checkout = new CheckOut();
         assertEquals(2, checkout.scanItem("A,50:B,30").size());
     }
-    
+
     @Test
     public void scan_one_item_and_print_its_price() {
         CheckOut checkout = new CheckOut();
         checkout.scanItem("A,50");
-        assertEquals(new Integer(50),checkout.getTotalPrice());
+        assertEquals(new Integer(50), checkout.getTotalPrice());
     }
-    
+
     @Test
     public void scan_2_items_and_print_total_price() {
         CheckOut checkout = new CheckOut();
         checkout.scanItem("A,50:B,30");
-        assertEquals(new Integer(80),checkout.getTotalPrice());
+        assertEquals(new Integer(80), checkout.getTotalPrice());
+    }
+
+    @Test
+    public void scan_3_same_items_and_print_total_offer_price() {
+        CheckOut checkout = new CheckOut();
+        checkout.scanItem("A,50,3,130:A,50,3,130:A,50,3,130");
+        assertEquals(new Integer(130), checkout.getTotalPrice());
     }
 }
