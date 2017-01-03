2450e707bc06fb26268cd0d33d86d37ae0b8a28a
vgupta0786 <hbticob@gmail.com>
2017-01-03 13:34:33 +0530

    refactored test

---
+++ CheckOutShould.java
@@ -0,0 +1,42 @@
+package cleancode;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class CheckOutShould {
+
+    @Test
+    public void scan_one_item() {
+        CheckOut checkout = new CheckOut();
+        assertNotNull(checkout.scanItem("A,50,0,0"));
+    }
+
+    @Test
+    public void scan_2_items() {
+        CheckOut checkout = new CheckOut();
+        assertEquals(2, checkout.scanItem("A,50,0,0:B,30,0,0").size());
+    }
+
+    @Test
+    public void scan_one_item_and_print_its_price() {
+        CheckOut checkout = new CheckOut();
+        checkout.scanItem("A,50,0,0");
+        assertEquals(new Integer(50), checkout.getTotalPrice());
+    }
+
+    @Test
+    public void scan_2_items_and_print_total_price() {
+        CheckOut checkout = new CheckOut();
+        checkout.scanItem("A,50,0,0:B,30,0,0");
+        assertEquals(new Integer(80), checkout.getTotalPrice());
+    }
+
+    @Test
+    public void scan_3_same_items_and_print_total_offer_price() {
+        CheckOut checkout = new CheckOut();
+        checkout.scanItem("A,50,3,130:A,50,3,0:A,50,3,0");
+        assertEquals(new Integer(130), checkout.getTotalPrice());
+    }
+}
