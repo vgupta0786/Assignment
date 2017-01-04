a0079c5931f2874aa004c93ee8677d6ba6c117db
vgupta0786 <hbticob@gmail.com>
2017-01-04 23:19:09 +0530

    Refactoring test class

--- CheckoutShould.java
+++ CheckoutShould.java
@@ -8,26 +8,29 @@ import org.junit.Test;
 public class CheckoutShould {
 
     private Checkout checkout;
+    private Item itemA;
+    private Item itemB;
 
     @Before
     public void setUp() {
         checkout = new Checkout();
+        itemA = new Item("A", 50);
+        itemB = new Item("B", 30);
     }
 
     @Test
     public void print_50_when_A_is_passed() {
-        assertEquals(new Integer(50), checkout.calculateTotalPrice(new Item("A", 50)));
+        assertEquals(new Integer(50), checkout.calculateTotalPrice(itemA));
     }
 
     @Test
     public void print_30_when_B_is_passed() {
-        assertEquals(new Integer(30), checkout.calculateTotalPrice(new Item("B", 30)));
+        assertEquals(new Integer(30), checkout.calculateTotalPrice(itemB));
     }
 
     @Test
     public void print_80_when_A_and_B_both_passed() {
-        assertEquals(new Integer(80),
-                checkout.calculateTotalPrice(new Item("A", 50), new Item("B", 30)));
+        assertEquals(new Integer(80), checkout.calculateTotalPrice(itemA, itemB));
     }
 
 }
