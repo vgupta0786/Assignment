bab4ff8a7fe1be6174ba30611aac5893d6eeee75
vgupta0786 <hbticob@gmail.com>
2017-01-04 23:18:01 +0530

    Green - used streams to sum items

--- Checkout.java
+++ Checkout.java
@@ -1,9 +1,12 @@
 package cleancode;
 
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.summingInt;
+
 public class Checkout {
 
-    public Integer calculateTotalPrice(Item item) {
-        return item.getPrice();
+    public Integer calculateTotalPrice(Item... items) {
+        return asList(items).stream().collect(summingInt(item -> item.getPrice()));
     }
 
 }
