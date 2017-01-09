a5b9e1f7e21679901a16cf52d32203df13d8f057
vgupta0786 <hbticob@gmail.com>
2017-01-09 15:16:07 +0530

    Refactoring - moved isfriend inside user

--- TripService.java
+++ TripService.java
@@ -13,7 +13,7 @@ public class TripService {
     public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
         User loggedUser = UserSession.getInstance().getLoggedUser();
         if (loggedUser != null) {
-            return user.getFriends().contains(loggedUser) ? TripDAO.findTripsByUser(user)
+            return user.isFriend(loggedUser) ? TripDAO.findTripsByUser(user)
                     : emptyList();
         } else {
             throw new UserNotLoggedInException();
